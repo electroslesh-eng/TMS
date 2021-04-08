ac28410ce4b75eb9c345949e4aa050e4e49be444
Илья <electroslesh94@gmail.com>
2021-04-08 18:37:36 +0300

    DZ4

---
+++ Lesson3/src/Homework4.java
@@ -0,0 +1,35 @@
+public class Homework4 {
+    public static void main(String[] args) {
+        System.out.println();
+        int[][] m = {{0},{0,0},{0,0,0},{0,0,0,0}};
+        for (int row = 0; row < m.length; row++){
+            for (int col=0; col<m[row].length; col++){
+                System.out.print(m[row][col] + " ");
+            }
+            System.out.println();
+            //всё что успел
+        }
+    }
+    }
+
+
+
+       //задача 8, поменять местами не смог
+//    {
+//    int[] mas = {4,5,0,23,77,0,8,9,101,2};
+//    int max = mas[0];
+//    for (int i = 1; i < mas.length;i++) max = Math.max(max, mas[i]);
+//    System.out.println(mas[max]);
+//    }
+//}
+        //задача 4
+//    int x = 5;
+//    if (x > 0) {
+//        System.out.println("Это положитеьное число, колличество цифр - 1");
+//    }else
+//        System.out.println("Это отрицательное число, колличество цифр - 1");
+//
+//    }
+//}
+
+
