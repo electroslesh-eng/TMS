2aadc421f2fc2ed538b3823ca71d25e4237e2e1a
Илья <electroslesh94@gmail.com>
2021-04-14 23:23:03 +0300

    DZ6

---
+++ Lesson66/src/com/home/lesson6/MilitaryOffice.java
@@ -0,0 +1,31 @@
+package com.home.lesson6;
+
+public class MilitaryOffice {
+    String name = "Илья Щербаков";
+    int age = 26;
+    String male = "male";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMale() {
+        return male;
+    }
+
+    public void setMale(String male) {
+        this.male = male;
+    }
+}
